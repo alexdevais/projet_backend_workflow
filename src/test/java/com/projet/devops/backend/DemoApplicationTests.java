ab5136369f8357085b43bc0f602ba4c441f6dae1
ouioui <alexdevais@gmail.com>
2026-07-02 12:49:47 +0200

    oui

--- src/test/java/com/projet/devops/backend/DemoApplicationTests.java
+++ src/test/java/com/projet/devops/backend/DemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.projet.devops.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
     @Test
